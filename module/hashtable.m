6aedef05c8eb00eea5fe60535ff7f6c0580dbbae
Alex Efros <powerman@powerman.name>
2010-03-13 08:30:25 +0200

    added initial support for deployment using mk and /contrib

---
+++ module/hashtable.m
@@ -0,0 +1,18 @@
+HashTable: module
+{
+	PATH: con "/contrib/hashtable/dis/lib/hashtable.dis";
+
+	HashNode: adt[T]{
+		key:string;
+		val:T;
+	};
+	Hash: adt[T]{
+		a:	array of list of ref HashNode[T];
+		new:	fn(size:int):ref Hash[T];
+		get:	fn(h:self ref Hash[T], key:string):T;
+		set:	fn(h:self ref Hash[T], key:string, val:T);
+		del:	fn(h:self ref Hash[T], key:string);
+		all:	fn(h:self ref Hash[T]): list of ref HashNode[T];
+	};
+};
+
